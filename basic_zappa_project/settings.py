b8caec702a70af8ab498f886b8380f65cda3509c
Daniel Newman <me@dtnewman.com>
2017-01-24 20:14:26 -0500

    update settings.py

--- basic_zappa_project/settings.py
+++ basic_zappa_project/settings.py
@@ -13,21 +13,21 @@ class Config(object):
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     CACHE_TYPE = 'simple'  # Can be "memcached", "redis", etc.
-    FLASKS3_BUCKET_NAME = '<BUCKET NAME HERE>' # TODO: Change me
+    FLASKS3_BUCKET_NAME = '<BUCKET NAME HERE>'  # TODO: Change me
 
 
 class Local(Config):
     """Local configuration."""
     ENV = 'lcl'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'postgresql://localhost:5432/basic_zappa_project'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_CONNECTION_STRING") or 'postgresql://localhost:5432/basic_zappa_project'
 
 
 class Development(Config):
     """Development configuration."""
     ENV = 'dev'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or '<DEVOLOPMENT DATABASE HERE>' # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_CONNECTION_STRING") or '<DEVOLOPMENT DATABASE HERE>' # TODO: Change me
     FLASKS3_ACTIVE = True
     FLASKS3_DEBUG = True
 
@@ -36,14 +36,14 @@ class Production(Config):
     """Production configuration."""
     ENV = 'prod'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or '<PRODUCTION DATABASE HERE>' # TODO: Change me
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_CONNECTION_STRING") or '<PRODUCTION DATABASE HERE>' # TODO: Change me
     DEBUG_TB_ENABLED = False  # Disable Debug toolbar
 
 
 class Test(Config):
     TESTING = True
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'postgresql://localhost:5432/basic_zappa_project_test'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_CONNECTION_STRING") or 'postgresql://localhost:5432/basic_zappa_project_test'
     BCRYPT_LOG_ROUNDS = 1  # For faster tests
     WTF_CSRF_ENABLED = False  # Allows form testing
 
