fad3e05b544c6bf84abd93659047e3bc7d0f770b
Daniel Newman <me@dtnewman.com>
2018-08-15 15:54:10 -0400

    fix indentation error

--- zappa_boilerplate/public/views.py
+++ zappa_boilerplate/public/views.py
@@ -69,3 +69,10 @@ def status():
     return jsonify({'status': 'ok'})
 
 
+@blueprint.route("/foo")
+def foo():
+    import requests
+    r = requests.get('http://castle.bookxchange.com/status')
+    return jsonify({'status': 'foo', 'external_status': r.json()})
+
+
